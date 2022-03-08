f853fd536949ba896bd67a7a533c3ca59ac066d6
Thiago Fonseca <thifonseca85@gmail.com>
2022-03-08 14:23:49 -0300

    project created

---
+++ backend/src/test/java/com/java/geocoding/GeocodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.geocoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
