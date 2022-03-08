6e9b41763e0dbedb8edaf42f1579182a8d290d7f
Thiago Fonseca <thifonseca85@gmail.com>
2022-03-08 18:25:21 -0300

    service, controller, open api

--- backend/src/main/java/com/java/geocoding/models/AddressResponse.java
+++ backend/src/main/java/com/java/geocoding/models/AddressResponse.java
@@ -16,6 +16,6 @@ import lombok.Setter;
 public class AddressResponse {
 	
    private Address address;
-   private List<LatLng> coordinates;
+   private List<LatLng> coords;
    private String result;
 }
---
+++ backend/src/main/java/com/java/geocoding/service/AddressService.java
@@ -0,0 +1,54 @@
+package com.java.geocoding.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import com.java.geocoding.calc.Calculation;
+import com.java.geocoding.models.Address;
+import com.java.geocoding.models.AddressResponse;
+
+@Service
+public class AddressService {
+
+	public AddressResponse getGeolocation(Address request , String key) {
+		
+		Calculation calc = new Calculation();
+		
+		List<LatLng> list = new ArrayList<>();
+		try {
+			
+		for(int i = 0; i < request.getAddress().size(); i++) {
+				GeoApiContext context = new GeoApiContext.Builder()
+						.apiKey(key)
+						.build();
+				GeocodingResult[] results = GeocodingApi.geocode(context,
+						request.getAddress().get(i)).await();
+				
+				list.add(results[0].geometry.location);
+			}
+		}catch (IOException e){
+            e.printStackTrace();
+		}catch (ApiException e){
+	           e.getLocalizedMessage();
+	        }catch (InterruptedException e){
+	            e.printStackTrace();
+	        }catch (ArrayIndexOutOfBoundsException e){
+	            e.getMessage();
+	        }
+            
+		
+			String result = calc.EuclidianCalc(list);
+			AddressResponse response = new AddressResponse(request, list, result);
+		return response;
+		
+	}
+
+	}
